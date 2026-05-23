ccaeb7931a608a67e919169e49ff8333adaff5f3
Nayan Dey <ndeynayan123@gmail.com>
2026-05-23 10:27:32 +0530

    updated rooms page and room card component

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,42 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.pixabay.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.metrolibrary.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.laportelibrary.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 's3.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'blog.bookmylibrary.in',
+      },
+      {
+        protocol: 'https',
+        hostname: 'd4804za1f1gw.cloudfront.source',
+      },
+      {
+        protocol: 'https',
+        hostname: 'd4804za1f1gw.cloudfront.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'library.princeton.edu',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
