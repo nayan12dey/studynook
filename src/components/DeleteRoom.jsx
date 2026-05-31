17637df13b0149ea90164d7c9faba4f6fd35cf0e
Nayan Dey <ndeynayan123@gmail.com>
2026-06-01 03:11:32 +0530

    added delete room functionality

--- src/components/DeleteRoom.jsx
+++ src/components/DeleteRoom.jsx
@@ -2,10 +2,27 @@
 "use client";
 
 import { AlertDialog, Button } from "@heroui/react";
+import { redirect } from "next/navigation";
 
 export function DeleteRoom({ room }) {
 
-    const { room_name } = room
+    const { _id, room_name } = room
+
+    const handleDelete = async () => {
+
+        const res = await fetch(`http://localhost:5000/rooms/${_id}`, {
+            method: "DELETE",
+            headers: {
+                "content-type": "application/json",
+            }
+        })
+
+        const data = await res.json()
+        console.log(data)
+        redirect("/rooms")
+    }
+
+
 
 
     return (
@@ -26,8 +43,8 @@ export function DeleteRoom({ room }) {
                             </p>
                         </AlertDialog.Body>
                         <AlertDialog.Footer>
-                            
-                            <Button slot="close" variant="danger" className={"w-full"}>
+
+                            <Button slot="close" variant="danger" className={"w-full"} onClick={handleDelete}>
                                 Delete
                             </Button>
                         </AlertDialog.Footer>
